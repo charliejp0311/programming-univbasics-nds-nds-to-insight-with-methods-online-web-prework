8ac845d17df087fcc47e6584799da78798f6ddcd
Charles Pelton <charliejp0311@gmail.com>
2020-01-16 15:46:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,10 +11,9 @@ def directors_totals(nds)
   while c1 < nds.length do
     dirctr = nds[c1][:name] 
     mny = gross_for_director(nds[c1])
-    result.merge(dirctr, mny)
     c1 += 1 
   end 
-  
+  result.merge(dirctr, mny)
   result
 end
 
